ab1f592cdae028e661133b134caeb243ae932e95
JulianGoodman91 <julian@stannp.com>
2025-06-12 13:05:52 +0100

    Added displayMessage function

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,12 +10,16 @@ let highScoreEl = document.querySelector('.highscore');
 let score = 20;
 let highScore = 0;
 
+const displayMessage = (msg) => {
+    message.textContent = msg;
+}
+
 document.querySelector('.again').addEventListener('click', () => {
     score = 20;
     guessValue.value = '';
     number.textContent = '?';
     scoreSelect.textContent = score;
-    message.textContent = 'Start guessing...';
+    displayMessage('Start guessing...');
     secretNumber = Math.trunc(Math.random() * 20) + 1;
     number.style.width = '15rem';
     document.querySelector('body').style.backgroundColor = '#222';
@@ -25,10 +29,10 @@ document.querySelector('.check').addEventListener('click', () => {
     const guess = Number(guessValue.value);
     
     if(!guess) {
-        message.textContent = "No Guess Given, please try again."
+        displayMessage("No Guess Given, please try again.");
 
     } else if (guess === secretNumber) {
-        message.textContent = "Well done, you got it right!"
+        displayMessage("Well done, you got it right!");
         document.querySelector('body').style.backgroundColor = '#60b347';
         number.style.width = '30rem';
         number.textContent = secretNumber;
@@ -38,15 +42,13 @@ document.querySelector('.check').addEventListener('click', () => {
             highScoreEl.textContent = highScore;
         }
 
-
-        // Guess is different
     } else if (guess !== secretNumber) {
         if  (score > 1) {
-            message.textContent = guess > secretNumber ? "Your guess was too high..." : "Your guess was too low...";
+            displayMessage(guess > secretNumber ? "Your guess was too high..." : "Your guess was too low...");
             score--;
             scoreSelect.textContent = score;
         } else {
-            message.textContent = "You Lost the game, better luck next time!";
+            displayMessage("You Lost the game, better luck next time!");
             scoreSelect.textContent = 0;
         }
     }
